96d44b9ee97109ef304191613fa5bcddae56e77b
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-03-07 09:46:51 +0300

    feat:add weather information in home screen

--- app/src/main/java/com/samkt/weathersavy/features/weather/presentation/HomeScreen.kt
+++ app/src/main/java/com/samkt/weathersavy/features/weather/presentation/HomeScreen.kt
@@ -1,15 +1,41 @@
 package com.samkt.weathersavy.features.weather.presentation
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.samkt.weathersavy.R
 import com.samkt.weathersavy.features.weather.data.model.CurrentWeather
 
 @Composable
@@ -45,7 +71,10 @@ fun HomeScreenError(
     errorMessage: String = "",
 ) {
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier =
+            modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center,
     ) {
         Text(text = errorMessage)
@@ -57,18 +86,160 @@ fun HomeScreenContent(
     modifier: Modifier = Modifier,
     currentWeather: CurrentWeather,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { paddingValues ->
+        Column(
+            modifier =
+                modifier
+                    .padding(paddingValues)
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    modifier = Modifier.size(32.dp),
+                    painter = painterResource(id = R.drawable.ic_location),
+                    contentDescription = null,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = currentWeather.name,
+                    style =
+                        MaterialTheme.typography.bodyLarge,
+                )
+            }
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                text = "March 06",
+                style =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontWeight = FontWeight.Normal,
+                    ),
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = R.drawable.ic_clouds),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = currentWeather.weather[0].main,
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Text(
+                text =
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle()) {
+                            append(
+                                currentWeather.main?.temp.toString(),
+                            )
+                        }
+                        withStyle(
+                            style =
+                                SpanStyle(
+                                    fontSize = 24.sp,
+                                    baselineShift = BaselineShift.Superscript,
+                                ),
+                        ) {
+                            append("°C")
+                        }
+                    },
+                style = MaterialTheme.typography.displayLarge,
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            WeatherAnalysis(
+                modifier = Modifier.padding(16.dp),
+                humidity = currentWeather.main?.humidity.toString(),
+                windSpeed = currentWeather.wind?.speed.toString(),
+                feelsLike = currentWeather.main?.feelsLike.toString(),
+            )
+        }
+    }
+}
+
+@Composable
+fun WeatherAnalysis(
+    modifier: Modifier = Modifier,
+    humidity: String = "",
+    windSpeed: String = "",
+    feelsLike: String = "",
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Text(text = currentWeather.name)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "humidity",
+            )
+            Text(
+                text = "HUMIDITY",
+                style = MaterialTheme.typography.labelLarge,
+            )
+            Text(
+                text = "$humidity %",
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "wind",
+            )
+            Text(
+                text = "WIND",
+                style = MaterialTheme.typography.labelLarge,
+            )
+            Text(
+                text = "$windSpeed km/h",
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = R.drawable.temp),
+                contentDescription = "temp",
+            )
+            Text(
+                text = "FEELS LIKE",
+                style = MaterialTheme.typography.labelLarge,
+            )
+            Text(
+                text = "$feelsLike°",
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
     }
 }
 
 @Composable
 fun HomeLoadingScreen(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier =
+            modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center,
     ) {
         CircularProgressIndicator()
--- app/src/main/java/com/samkt/weathersavy/ui/theme/Type.kt
+++ app/src/main/java/com/samkt/weathersavy/ui/theme/Type.kt
@@ -2,33 +2,49 @@ package com.samkt.weathersavy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.samkt.weathersavy.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val poppins =
+    FontFamily(
+        Font(R.font.poppins_light, FontWeight.Light),
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_medium, FontWeight.Medium),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold),
+        Font(R.font.poppins_bold, FontWeight.Bold),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+val Typography =
+    Typography(
+        labelLarge =
+            TextStyle(
+                fontFamily = poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 16.sp,
+            ),
+        bodyLarge =
+            TextStyle(
+                fontFamily = poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = 18.sp,
+                lineHeight = 16.sp,
+            ),
+        titleLarge =
+            TextStyle(
+                fontFamily = poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                lineHeight = 47.sp,
+            ),
+        displayLarge =
+            TextStyle(
+                fontFamily = poppins,
+                fontWeight = FontWeight.Medium,
+                fontSize = 86.sp,
+                lineHeight = 100.sp,
+            ),
     )
-    */
-)

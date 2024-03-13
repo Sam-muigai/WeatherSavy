bdd6d1a6574192099605731c4c30a5a89be26597
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-03-13 19:36:49 +0300

    feat:added room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,5 +90,11 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
+    // Lottie
     implementation("com.airbnb.android:lottie-compose:4.0.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
